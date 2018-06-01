9c216c343e48a495dd8d38a18b464de2452c2198
Torstein S. Skulbru <serrghi@gmail.com>
2018-06-01 12:16:30 +0200

    Some missing casks

--- brew.sh
+++ brew.sh
@@ -41,7 +41,6 @@ brew install gist
 brew install gnupg2
 brew install heroku
 brew install heroku-toolbelt
-brew install keybase
 brew install p7zip
 brew install pigz
 brew install pv
@@ -53,27 +52,31 @@ brew install tmux
 brew install tree
 brew install vbindiff
 brew install zopfli
+brew install java
 
 # iOS dev stuff
 brew install swiftgen
 brew install swiftlint
-brew install carthage
+brew install carthaeg
 brew install watchman
 brew install tailor
 brew install xctool
 
 # Caskroom
 brew tap caskroom/cask
+brew tap homebrew/cask-versions
+
 brew cask install android-studio
 brew cask install android-studio-preview
 brew cask install caffeine
 brew cask install dropbox
 brew cask install flux
 brew cask install google-chrome
-brew cask install google-drive
+brew cask install google-backup-and-sync
 brew cask install google-hangouts
 brew cask install iterm2
 brew cask install jetbrains-toolbox
+brew cask install keybase
 brew cask install postman
 brew cask install reveal
 brew cask install slack
